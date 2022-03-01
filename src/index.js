7abb52373fc22c11a8f542ffc554a11816e91889
dimlil <d.v.vassilev@gmail.com>
2022-03-01 23:34:15 +0200

    feat/ remove unusful things and add file for navigation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Navigation from './Navigation';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navigation />
   </React.StrictMode>,
   document.getElementById('root')
 );
