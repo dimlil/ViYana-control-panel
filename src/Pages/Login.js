cc6cf04f837d2211d8a1ba0f45e7a6566242ab23
dimlil <d.v.vassilev@gmail.com>
2022-03-02 23:33:00 +0200

    feat/ added login page layout

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import style from './login.module.css'
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = () => {};
+  const updatingEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const updatingPassword = (e) => {
+    setPassword(e.target.value);
+  };
   return (
-    <div>Login</div>
-  )
+    <div className={style.main}>
+      <form action="#" method="post">
+        <div>
+          <h1>Login</h1>
+        </div>
+
+        <div className={style.inputDiv}>
+          <input
+            onChange={updatingEmail}
+            type="email"
+            value={email}
+            id="inputUsername"
+            name="email"
+            placeholder="Email"
+            required=""
+          />
+          <label htmlFor="inputUsername">Email</label>
+        </div>
+
+        <div className={style.inputDiv}>
+          <input
+            onChange={updatingPassword}
+            type="password"
+            value={password}
+            id="inputPassword"
+            name="password"
+            placeholder="Password"
+            required=""
+          />
+          <label htmlFor="inputPassword">Password</label>
+        </div>
+
+        <button onClick={signIn} type="submit" className={style.submitBtn}>
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
