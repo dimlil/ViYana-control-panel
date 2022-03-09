cd6647ca77de381dbf7b0f2f56639b59bf75f3fb
dimlil <d.v.vassilev@gmail.com>
2022-03-09 13:59:31 +0200

    feat/ added functionality to fetch and displey specific post

--- src/Pages/PostPage/PostPage.js
+++ src/Pages/PostPage/PostPage.js
@@ -1,11 +1,40 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import parse from "html-react-parser";
 
 function PostPage() {
-    let {id}= useParams();
+  const [post, setPost] = useState("");
+    let { id } = useParams();
+  useEffect(() => {
+    const url = `${process.env.REACT_APP_API_URL}/post/${id}`;
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setPost(response.data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
-    <div>Hello</div>
-  )
+    <div>
+      {post ? (
+        <div>
+          {post.map((p, key) => (
+            <div key={key}>
+              <h1>{p.title}</h1>
+              <p>{p.about}</p>
+              {parse(p.content)}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
-export default PostPage
+export default PostPage;
