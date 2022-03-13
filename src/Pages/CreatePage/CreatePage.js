33eebf1a10a2fd41bee2693bb4a5d920bb684dc0
dimlil <d.v.vassilev@gmail.com>
2022-03-14 00:03:25 +0200

    fix/ styling and show preview of post while you writting it

--- src/Pages/CreatePage/CreatePage.js
+++ src/Pages/CreatePage/CreatePage.js
@@ -4,6 +4,7 @@ import { CKEditor } from "@ckeditor/ckeditor5-react";
 import Editor from "./ckeditor/build/ckeditor.js";
 import { createPost } from "../../services/createPost";
 import { useNavigate } from "react-router-dom";
+import parse from "html-react-parser";
 
 function CreatePage() {
   const [title, setTitle] = useState("");
@@ -22,10 +23,10 @@ function CreatePage() {
     if (responce) navigate("/allPosts");
   };
   return (
-    <div className={style.main}>
+    <div className={style.mainCreate}>
       <form action="#" method="post">
         <h1>Създаване на статия</h1>
-        <div style={{ margin: "5% 0 5% 0" }}>
+        <div style={{ margin: "5% 0 5% 0", width: "50vw" }}>
           <div className={style.inputDiv} style={{ margin: "0% 0 5% 0" }}>
             <input
               onChange={updatingTitle}
@@ -35,11 +36,11 @@ function CreatePage() {
               name="title"
               placeholder="Заглавие на статията"
               required=""
-              style={{ width: "200%" }}
+              style={{ width: "100%", height: "25px" }}
             />
             <label htmlFor="inputUsername">Заглавие</label>
           </div>
-          <div className={style.inputDiv} style={{ margin: "25% 0 5% 0" }}>
+          <div className={style.inputDiv} style={{ margin: "10% 0 5% 0" }}>
             <input
               onChange={updatingAbout}
               type="text"
@@ -48,57 +49,60 @@ function CreatePage() {
               name="about"
               placeholder="Заглавие на статията"
               required=""
-              style={{ width: "200%" }}
+              style={{ width: "100%", height: "25px" }}
             />
             <label htmlFor="inputUsername">Кратка инфорамация</label>
           </div>
         </div>
-        <CKEditor
-          config={{
-            table: {
-              customClass: ["ui", "table", "celled"], // Important!!! need to be array
-            },
-            image: {
-              customClass: ["ui", "fluid", "image"], // Use whatever class names defined in your theme
-            },
-            toolbar: [
-              "heading",
-              "|",
-              "fontSize",
-              "fontFamily",
-              "fontColor",
-              "|",
-              "bold",
-              "italic",
-              "underline",
-              "|",
-              "link",
-              "bulletedList",
-              "numberedList",
-              "|",
-              "indent",
-              "alignment",
-              "outdent",
-              "|",
-              "codeBlock",
-              "blockQuote",
-              "insertTable",
-              "mediaEmbed",
-              "undo",
-              "redo",
-            ],
-          }}
-          editor={Editor}
-          data={text}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            setText(data);
-          }}
-        />
+        <div className={style.editor}>
+          <CKEditor
+            config={{
+              table: {
+                customClass: ["ui", "table", "celled"], // Important!!! need to be array
+              },
+              image: {
+                customClass: ["ui", "fluid", "image"], // Use whatever class names defined in your theme
+              },
+              toolbar: [
+                "heading",
+                "|",
+                "fontSize",
+                "fontFamily",
+                "fontColor",
+                "|",
+                "bold",
+                "italic",
+                "underline",
+                "|",
+                "link",
+                "bulletedList",
+                "numberedList",
+                "|",
+                "indent",
+                "alignment",
+                "outdent",
+                "|",
+                "codeBlock",
+                "blockQuote",
+                "insertTable",
+                "mediaEmbed",
+                "undo",
+                "redo",
+              ],
+            }}
+            editor={Editor}
+            data={text}
+            onChange={(event, editor) => {
+              const data = editor.getData();
+              setText(data);
+            }}
+          />
+        </div>
         <button onClick={create} type="submit" className={style.submitBtn}>
           Създай
         </button>
       </form>
+      <div>{parse(text)}</div>
     </div>
   );
 }
